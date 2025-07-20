e2ec41bef4f8e93598b9afad81fdbc7d22be56da
Yernat <118939623+yernatturarbekov@users.noreply.github.com>
2025-07-20 22:34:39 +0500

    Main.java

--- src/yernat/hw2/Main.java
+++ src/yernat/hw2/Main.java
@@ -1,4 +1,19 @@
 package yernat.hw2;
 
 public class Main {
+    public static void main(String[] args){
+        Triangle myTriangle = new Triangle(4,5,6);
+        Rectangle myRectangle = new Rectangle(4,5);
+        Square mySquare = new Square(4);
+
+        myTriangle.getPerimeter();
+        myTriangle.getArea();
+
+        myRectangle.getPerimeter();
+        myRectangle.getArea();
+
+        mySquare.getPerimeter();
+        mySquare.getArea();
+
+    }
 }
