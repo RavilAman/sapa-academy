1a7f98865c910345173b27e61afd473d5185aef9
Zhandos Badeshov <jandos.badeshov@gmail.com>
2025-07-20 10:20:46 +0500

    HomeWork #2

---
+++ src/badeshov/hw2/Rectangle.java
@@ -0,0 +1,19 @@
+package badeshov.hw2;
+
+public class Rectangle {
+    public void perimeterOfRectangle() {
+
+        double a = 5;
+        double b = 13;
+        double perimeter = 2 * a + 2 * b;
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
+
+    public void areaOfRectangle() {
+
+        double a = 5;
+        double b = 13;
+        double area = a * b;
+        System.out.println("Area of Rectangle = " + area);
+    }
+}
