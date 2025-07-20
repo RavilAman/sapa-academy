dd74f2c01b3b2e837245271e679fbca607f98c0d
Turarbekov Yernat <turarbekov.yernat@gmail.com>
2025-07-20 22:46:00 +0500

    new hw-2

--- src/yernat/hw2/Rectangle.java
+++ src/yernat/hw2/Rectangle.java
@@ -1,4 +1,24 @@
 package yernat.hw2;
 
+import static java.lang.Math.sqrt;
+
 public class Rectangle {
+    double a, b;
+
+    public Rectangle(double a, double b){
+        this.a = a;
+        this.b = b;
+
+    }
+
+    public void getPerimeter(){
+        double plus = a + b;
+        System.out.println(plus);
+
+    }
+    public void getArea(){
+        double multiple = a * b;
+        System.out.println(multiple);
+    }
+
 }
--- src/yernat/hw2/Triangle.java
+++ src/yernat/hw2/Triangle.java
@@ -1,4 +1,28 @@
 package yernat.hw2;
 
+import javax.crypto.spec.PSource;
+
+import static java.lang.Math.sqrt;
+
 public class Triangle {
+    double a, b, c;
+
+    public Triangle(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+
+    public void getPerimeter(){
+        double plus = a + b + c;
+        System.out.println(plus);
+
+    }
+    public void getArea(){
+        double d = (a+b+c)/2;
+        double multiple = sqrt(d * (d-a) * (d-b) * (d-c));
+        System.out.println(multiple);
+    }
 }
+
