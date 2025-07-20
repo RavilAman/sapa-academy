343c07353d290832a032d7bcaff6ff3f0549229c
Yernat <118939623+yernatturarbekov@users.noreply.github.com>
2025-07-20 22:35:39 +0500

    Square.java

--- src/yernat/hw2/Square.java
+++ src/yernat/hw2/Square.java
@@ -1,4 +1,23 @@
 package yernat.hw2;
 
+import static java.lang.Math.sqrt;
+
 public class Square {
+
+    double a;
+
+    public Square(double a){
+        this.a = a;
+    }
+
+    public void getPerimeter(){
+        double plus = a + a;
+        System.out.println(plus);
+
+
+    }
+    public void getArea(){
+        double multiple = a * a;
+        System.out.println(multiple);
+    }
 }
