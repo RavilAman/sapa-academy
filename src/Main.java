bc580ba08c2e280cfc77423892a51faac0c4a5e0
Ravil Amangeldiuly <ravil.aman.job@gmail.com>
2025-07-22 14:57:29 +0500

    abstract class and interface example

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import example.lesson.four.AbstractPolygon;
-import example.lesson.four.Square;
+import example.lesson.four.Rectangle;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        AbstractPolygon square = new Square();
-        square.calculatePerimeter();
-        square.calculateArea();
+        AbstractPolygon polygon = new Rectangle();
+        polygon.calculatePerimeter();
+        polygon.calculateArea();
     }
 }
