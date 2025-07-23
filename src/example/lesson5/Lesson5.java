cce76b46a2a2cdb5ad3f89feb571b6486a72d70d
a.ulykbekov <a.ulykbekov@jusan.kz>
2025-07-23 20:44:09 +0500

    feat(lesson-5): add lesson 5

---
+++ src/example/lesson5/Lesson5.java
@@ -0,0 +1,23 @@
+package example.lesson5;
+
+public class Lesson5 {
+
+  public static void main(String[] args) {
+
+    Parent parent = new Parent("Papa", 50);
+    Parent parent2 = new Parent("Mama", 50);
+    Arsen arsen = new Arsen("Arsen", 20, "Dvornik");
+
+    Parent[] family = new Parent[]{parent, arsen, parent2};
+
+    for (Parent p : family) {
+      System.out.print("Name = " + p.getName() + " Age  = " + p.getAge() + " ");
+
+      if (p instanceof Arsen) {
+        System.out.print("profession = " + ((Arsen) p).getProfession());
+      }
+
+      System.out.println();
+    }
+  }
+}
