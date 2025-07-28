2d56325aa2a3b3619dd250e9184ae56047b5fba2
Turarbekov Yernat <turarbekov.yernat@gmail.com>
2025-07-28 12:06:10 +0500

    hw5 after resolving

--- src/example/lesson6/Test.java
+++ src/example/lesson6/Test.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 
 public class Test {
 
-  private static Logger logger = Logger.getLogger(Test.class.getName());
+  public static Logger logger = Logger.getLogger(Test.class.getName());
 
   public static void main(String[] args) {
 
