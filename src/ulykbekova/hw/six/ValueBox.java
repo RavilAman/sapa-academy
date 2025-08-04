f9add5d2cc5309ba49e743c8cb88d3b5ccf1be11
Erkezhan <eulykbekova@gmail.com>
2025-08-04 16:45:52 +0500

    feat(HW-6): Обращение

--- src/ulykbekova/hw/six/ValueBox.java
+++ src/ulykbekova/hw/six/ValueBox.java
@@ -1,26 +1,35 @@
 package ulykbekova.hw.six;
 
-public class ValueBox <T>{
+import java.util.Scanner;
+
+public class ValueBox<T> {
     public T value;
 
-    public ValueBox(T value){
-        this.value=value;
+    public ValueBox(T value) {
+        this.value = value;
     }
-    public T getValue(){
+
+    public T getValue() {
         return value;
     }
-    public void setValue(T value){
-        this.value=value;
+
+    public void setValue(T value) {
+        this.value = value;
     }
-    public boolean isEguals(ValueBox<T> a ){
+
+    public boolean isEguals(ValueBox<T> a) {
         return value.equals(a.getValue());
     }
 
-    public static <T extends Comparable<T>> T max(ValueBox<T> first, ValueBox<T> second){
-        if (first.getValue().compareTo(second.getValue()) >=0){
+    public static <T extends Comparable<T>> T max(ValueBox<T> first, ValueBox<T> second) {
+        if (first.getValue().compareTo(second.getValue()) > 0) {
+            return first.getValue();
+        } else if (first.getValue().compareTo(second.getValue()) == 0) {
+            System.out.println("числа равны ");
             return first.getValue();
         } else {
             return second.getValue();
         }
     }
 }
+
