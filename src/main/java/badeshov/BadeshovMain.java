d72765a3b2f27b1de49a07c9e00c83d3d62a4dcc
Zhandos Badeshov <jandos.badeshov@gmail.com>
2025-08-06 19:47:45 +0500

    черновик

--- src/main/java/badeshov/BadeshovMain.java
+++ src/main/java/badeshov/BadeshovMain.java
@@ -1,44 +1,37 @@
 package badeshov;
 
-import main.java.badeshov.hw7.SuperMarket;
+import example.lesson9.hw.model.WeatherResponse;
+import example.lesson9.hw.service.WeatherDataService;
+import org.w3c.dom.Document;
 
-import java.util.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class BadeshovMain {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        List<WeatherDataService> weatherDataServiceList = new ArrayList<>();
 
-        ArrayList<String> pokupka = new ArrayList<>();
+//        String apiKey; efc3592e2e7a43a3b38105635250408
+//        String city; на английском
+//        int days; 'год-месяц-день час:минута', например: 2025-08-01 22:00
+        WeatherDataService service = new WeatherDataService();
+        service.apiKey = efc3592e2e7a43a3b38105635250408;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название города на английском языке: ");
+        String city = scanner.nextLine();
+        System.out.println("Насколько дней хотите узнать прогноз погоды? (максимально до 14 дней) ");
 
-        while (true) {
-            System.out.println("Введите товар (или /stop для завершения): ");
-            String next = input.next();
-
-            if (next.equals("stop")) {
-                HashMap<String, Integer> map = new HashMap<>();
-                for (String s : pokupka) {
-                    if (map.containsKey(s)) {
-                        Integer i = map.get(s) + 1;
-                        map.put(s, i);
-                    } else {
-                        map.put(s, 1);
-                    }
-                }
-                System.out.println(map);
-
-                System.out.println("Хотите добавить ещё товары? (да/нет): ");
-                next = input.next();
-                if (next.equals("да")) {
-                    continue;
-                } else {
-                    break;
-                }
-            }
-
-            pokupka.add(next);
-        }
+        // вам нужно передавать в этот метод свой api key,
+        // город что ввел пользователь и
+        // насколько дней человек хочет узнать погоду,
+        // 1 на сегодняшний день, 2 на сегодня и завтра получим данные о погоде, и т.д. макс 14
+//        service.getWeatherData(apiKey, city, days);
     }
 }
