341909115c4e23d2e411018278f2df7be5a5d7f7
Yerassyl <erasyl.adletyly@gmail.com>
2025-08-06 13:42:52 +0500

    tursynbayev-hw-7

--- src/yrs/Shopping.java
+++ src/yrs/Shopping.java
@@ -5,37 +5,34 @@ import java.util.logging.Logger;
 
 public class Shopping {
     private static final Logger logger = Logger.getLogger(Shopping.class.getName());
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> list = new ArrayList<>();
         boolean cont = true;
-        while(cont){
+        while (cont) {
             logger.info("Enter a product (or enter 'stop'):");
-            while (true){
-                String choo = scanner.nextLine();
-                if (choo.equalsIgnoreCase("stop")){
-                    break;
+            String choo = scanner.nextLine();
+            if (choo.equalsIgnoreCase("stop")) {
+                Map<String, Integer> count = new HashMap<>();
+                for (String item : list) {
+                    count.put(item, count.getOrDefault(item, 0) + 1);
                 }
-                if (!choo.isEmpty()){
-                    list.add(choo);
+                logger.info("results of purchase");
+                for (Map.Entry<String, Integer> entry : count.entrySet()) {
+                    logger.info(entry.getKey() + " — " + entry.getValue());
                 }
-            }
-            Map<String, Integer> count = new HashMap<>();
-            for (String item:list){
-                count.put(item, count.getOrDefault(item,0)+1);
-            }
-            logger.info("results of pusrchase");
-            for (Map.Entry<String, Integer> entry : count.entrySet()) {
-                logger.info(entry.getKey() + " — " + entry.getValue());
-            }
-            logger.info("Do you want to add any products: ");
-            String answer = scanner.nextLine();
-            if (!answer.equalsIgnoreCase("yes")) {
-                cont = false;
-                System.out.println("Thanks for shopping");
+
+                logger.info("Do you want to add any products: ");
+                String answer = scanner.nextLine();
+                if (!answer.equalsIgnoreCase("yes")) {
+                    cont = false;
+                    System.out.println("Thanks for shopping");
+                }
+            } else if (!choo.isEmpty()) {
+                list.add(choo);
             }
         }
     }
 }
-
 
