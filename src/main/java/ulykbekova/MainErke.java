8c402727de8e2fd6700e8bd4acdadde29e091c8e
Erkezhan <eulykbekova@gmail.com>
2025-08-10 19:32:48 +0500

    feat(HW-7): коллекция изменила

--- src/main/java/ulykbekova/MainErke.java
+++ src/main/java/ulykbekova/MainErke.java
@@ -6,7 +6,6 @@ import java.util.*;
 import java.util.logging.Logger;
 
 public class MainErke {
-    private static MessageHeader entry;
     private static final Logger logger = Logger.getLogger(MainErke.class.getName());
 
     public static void main(String[] args) {
